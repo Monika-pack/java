aa82bd07babf68b1f264f007bdc811254c4cc3d8
Monika-pack <98116508+Monika-pack@users.noreply.github.com>
2022-02-03 19:50:00 +0530

    Add files via upload

---
+++ ExceptionalSyncronized.java
@@ -0,0 +1,47 @@
+package first;
+class Table{
+		 synchronized void print(int n,String g) {
+				for(int i=1;i<=10;i++) {
+				System.out.println("n x"+i+"="+n*i);
+			}
+				System.out.println("hii");
+			}
+	}
+	class FirstThread extends Thread{
+		Table t;
+		public FirstThread(Table te){
+			this.t=te;
+		}
+		public void run() {
+			t.print(5,"Hellow");
+		}
+	}
+
+	class SecondThread extends Thread{
+		Table t;
+		public SecondThread(Table te) {
+			this.t=te;
+		}
+
+		public void run() {
+			t.print(10,"Hi");
+			
+		}
+	}
+	public class ExceptionalSyncronized {
+
+		public static void main(String[] args) throws InterruptedException {
+			Table te = new Table();//common object
+			FirstThread t1 = new FirstThread(te);
+			SecondThread t2= new SecondThread(te);
+			t1.start();
+			
+			t2.start();
+		}
+	}
+	
+
+
+	
+
+
