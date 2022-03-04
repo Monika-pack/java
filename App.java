a6ef2bfd5fa13742848979b94d6797dbfa0ce866
Monika-pack <98116508+Monika-pack@users.noreply.github.com>
2022-03-04 10:54:15 -0500

    Add files via upload

---
+++ App.java
@@ -0,0 +1,42 @@
+package oneTOmany_manyTOone;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
+public class App {
+
+	public static void main(String[] args) {
+		//Laptop 
+				Laptop lob=new Laptop();
+				lob.setLid(2);
+				lob.setLname("Samsung23");
+						
+				Student sob=new Student();
+				sob.setRollno(12);
+				sob.setName("Swetha");
+				sob.setMarks(457);
+				//sob.setLp(lob);
+				sob.getLp().add(lob);
+				lob.setSt(sob);
+				
+				Configuration con=new Configuration().configure().addAnnotatedClass(Laptop.class).addAnnotatedClass(Student.class);
+				ServiceRegistry reg = new ServiceRegistryBuilder().applySettings(con.getProperties()).buildServiceRegistry();
+				
+				//SessionFactory sf = con.buildSessionFactory(); //deprecated
+				SessionFactory sf = con.buildSessionFactory(reg); //not deprecated
+				Session session = sf.openSession();
+				Transaction tx=session.beginTransaction();
+				session.save(lob);
+				session.save(sob);
+				tx.commit();
+	}
+}
+
+		
+
+	
+
+
