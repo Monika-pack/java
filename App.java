12bbdd8de21044bf3df742357bf7d27f36945f58
Monika-pack <98116508+Monika-pack@users.noreply.github.com>
2022-03-11 03:02:46 -0500

    Add files via upload

--- App.java
+++ App.java
@@ -1,4 +1,4 @@
-package lapppstudent;
+package onetooneRelations;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -6,20 +6,31 @@ import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 import org.hibernate.service.ServiceRegistryBuilder;
 
-
 public class App {
 
 	public static void main(String[] args) {
+		
 		//Laptop 
 				Laptop lob=new Laptop();
 				lob.setLid(3);
 				lob.setLname("Samsung1");
-						
+				
+				Laptop lob2=new Laptop();
+				lob.setLid2(3);
+				lob.setLname2("peacock2");
+				
 				Student sob=new Student();
 				sob.setRollno(13);
 				sob.setName("Swetha1");
 				sob.setMarks(457);
 				sob.setLp(lob);
+				
+				Student sob2=new Student();
+				sob.setRollno(13);
+				sob.setName("Swetha1");
+				sob.setMarks(457);
+				sob.setLp(lob);
+				
 				Configuration con=new Configuration().configure().addAnnotatedClass(Laptop.class).addAnnotatedClass(Student.class);
 				ServiceRegistry reg = new ServiceRegistryBuilder().applySettings(con.getProperties()).buildServiceRegistry();
 				
@@ -30,10 +41,10 @@ public class App {
 				session.save(lob);
 				session.save(sob);
 				tx.commit();
-			}
-
+			
+	
 		
-
+	
 	}
 
-
+}
--- Laptop.java
+++ Laptop.java
@@ -1,30 +1,50 @@
-package lapppstudent;
+package onetooneRelations;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+@Entity
+
 public class Laptop {
-
-	//public static void main(String[] args) {
-		//@Entity
-		//public class Laptop {
-			@Id
-			private int lid;
-			private String lname;
-			public int getLid() {
-				return lid;
-			}
-			public void setLid(int lid) {
-				this.lid = lid;
-			}
-			public String getLname() {
-				return lname;
-			}
-			public void setLname(String lname) {
-				this.lname = lname;
-			}
-			
-		}
-
+	@Id
+	private int lid;
+	private int lid2;
+	private String lname;
+	private String lname2;
+	
+	public int getLid() {
+		return lid;
+	}
+	public void setLid(int lid) {
+		this.lid = lid;
+	}
+	public int getLid2() {
+		return lid2;
+	}
+	public void setLid2(int lid2) {
+		this.lid2 = lid2;
+	}
+	
+	
+	
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
 	
 
+
+
+
+public String getLname2() {
+	return lname2;
+}
+public void setLname2(String lname2) {
+	this.lname2 = lname2;
+}
+
+}
 
