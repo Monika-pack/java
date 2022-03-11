1c64782418041f0ff95d214f863c9095897c0a2a
Monika-pack <98116508+Monika-pack@users.noreply.github.com>
2022-03-11 03:14:27 -0500

    Add files via upload

--- MainApp.java
+++ MainApp.java
@@ -1,25 +1,28 @@
-package spring_inheritance;
+package spring_injection_setget;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-//import org.springframework.context.support.AbstractApplicationContext;
+
+
+
+
+
 public class MainApp {
-
-	private static ApplicationContext ctx;
-
 	public static void main(String[] args) {
-	ctx = new ClassPathXmlApplicationContext("spring.xml");
-	
-	HellowWorld objA =(HellowWorld)  ctx.getBean("in");
-	objA.getMessages1();
-		objA.getMessages2();
-			
-	
-			HellowIndia objB =(HellowIndia)  ctx.getBean("in");
-	objB.getMessages1();
-		objB.getMessages2();
-			objB.getMessages3();
-			
-	}
 
+
+	{
+		
+		
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+		Student sob = (Student) ctx.getBean("stob");
+		sob.display();
+		System.out.println(sob);
+		
+	}
+	
+
+	}
 }
+
+
--- Student.java
+++ Student.java
@@ -1,9 +1,31 @@
-package spring_hellow;
+package spring_injection_setget;
 
 public class Student {
-	public void display() {
-		System.out.println("hellow my first spring");
-		System.out.println("very fastest spring project ");
+	private int id;
+	private String name;
+	private float fees;
+	public int getId() {
+		return id;
 	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public float getFees() {
+		return fees;
+	}
+	public void setFees(float fees) {
+		this.fees = fees;
+	}
+	public void display() {
+		System.out.println("Name"+name);
+		System.out.println("id"+id);
+		System.out.println("Fees"+fees);
 
-}
+	}
+	}
