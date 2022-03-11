e1b8f574fdae5555a0dd005a31435856f649261f
Monika-pack <98116508+Monika-pack@users.noreply.github.com>
2022-03-11 03:10:13 -0500

    Add files via upload

--- MainApp.java
+++ MainApp.java
@@ -1,9 +1,24 @@
-package persistance;
+package constructor_injection;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+
+
 
 public class MainApp {
-
 	public static void main(String[] args) {
+
+
+	{
+		
+		
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+		Student sob = (Student) ctx.getBean("stob");
+		sob.display();
+		System.out.println(sob);
 		
 	}
 
+	}
 }
